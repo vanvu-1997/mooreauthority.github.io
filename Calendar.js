9b3aa2d8982e944ba0c602810d054333dfe1e1ef
Van Vu <vanvu_@outlook.com>
2024-08-31 16:55:59 -0400

    Calendar

---
+++ Calendar.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        // Optional: Add other configurations here
+    });
+
+    calendar.render();
+});
